60249ce743c0096087bc09d27f80e3d07dbabff4
René Samselnig <Rene.Samselnig@skiline.cc>
2013-10-16 12:46:12 +0200

    Adds more tests.

---
+++ test/resources/testcase-15.sh
@@ -0,0 +1,1 @@
+db-navigator.py -s skiline@localhost/skiline/api_key/669ba310c9fb11e2aa1fd702d6148dbf/
--- test/test.sh
+++ test/test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 STATUS=0
+TOTAL=$#
+OK=0
+FAILED=0
 
 while (( "$#" )); do
 	TESTCASE=$(basename ${1%.sh})
@@ -14,6 +17,7 @@ while (( "$#" )); do
 	DIFF=$(diff -u $EXPECTED $ACTUAL)
 
 	if [ "$DIFF" != "" ]; then
+		FAILED=$((FAILED+1))
 		echo FAILED
 		echo $SCRIPT
 		if [ "$(which colordiff)" != "" ]; then
@@ -25,10 +29,14 @@ while (( "$#" )); do
 		fi
 		STATUS=-1
 	else
+		OK=$((OK+1))
 		echo OK
 	fi
 
 	shift
 done
+
+echo "Tests run: $TOTAL, failed: $FAILED"
+echo
 
 exit $STATUS
