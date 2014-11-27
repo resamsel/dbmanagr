b9631e2c20c9fd65f6233590e205355e7bbd7642
René Samselnig <Rene.Samselnig@skiline.cc>
2014-11-27 07:30:31 +0100

    Adds tests for options.restriction and .format_value.

--- src/dbnav/options.py
+++ src/dbnav/options.py
@@ -22,6 +22,65 @@ def escape_keyword(keyword):
     return keyword
 
 
+def restriction(
+        alias, column, operator, value, map_null_operator=True):
+    if not column:
+        raise Exception('Parameter column may not be None!')
+    if operator in ['=', '!='] and (value is None or value == 'null'):
+        if map_null_operator:
+            operator = {
+                '=': 'is',
+                '!=': 'is not'
+            }.get(operator)
+        value = None
+    if column.table and alias is not None:
+        return u"{0}.{1} {2} {3}".format(
+            alias,
+            escape_keyword(column.name),
+            operator,
+            format_value(column, value))
+    return u'{0} {1} {2}'.format(
+        escape_keyword(column.name),
+        operator,
+        format_value(column, value))
+
+
+def format_value(column, value):
+    if value is None or (type(value) is float and math.isnan(value)):
+        return 'null'
+    if type(value) is list:
+        return '({0})'.format(
+            ','.join([format_value(column, v) for v in value]))
+    if type(value) in [datetime.datetime, datetime.date, datetime.time]:
+        return "'%s'" % value
+    if type(value) is buffer:
+        return u"'[BLOB]'"
+    if column is None:
+        try:
+            return '%d' % int(value)
+        except ValueError:
+            return u"'%s'" % value
+    if (isinstance(column.type, Boolean)
+            and (type(value) is bool or value in ['true', 'false'])):
+        return '%s' % str(value).lower()
+    if isinstance(column.type, Float):
+        try:
+            return '%f' % float(value)
+        except ValueError:
+            pass
+    if isinstance(column.type, Integer):
+        try:
+            return '%d' % int(value)
+        except ValueError:
+            pass
+    if isinstance(column.type, TIMESTAMP):
+        try:
+            return '%d' % int(value)
+        except ValueError:
+            pass
+    return u"'%s'" % value.replace('%', '%%').replace("'", "''")
+
+
 def parse_filter(s):
     filter = []
     for term in s.split(AND_OPERATOR):
@@ -90,60 +149,10 @@ class Options:
 
     def restriction(
             self, alias, column, operator, value, map_null_operator=True):
-        if not column:
-            raise Exception('Column is None!')
-        if column.table and alias is not None:
-            return u"{0}.{1} {2} {3}".format(
-                alias,
-                self.escape_keyword(column.name),
-                operator,
-                self.format_value(column, value))
-        if operator in ['=', '!='] and (value == 'null' or value is None):
-            if map_null_operator:
-                operator = {
-                    '=': 'is',
-                    '!=': 'is not'
-                }.get(operator)
-            value = None
-        return u'{0} {1} {2}'.format(
-            self.escape_keyword(column.name),
-            operator,
-            self.format_value(column, value))
+        return restriction(alias, column, operator, value, map_null_operator)
 
     def format_value(self, column, value):
-        if value is None or (type(value) is float and math.isnan(value)):
-            return 'null'
-        if type(value) is list:
-            return '({0})'.format(
-                ','.join([self.format_value(column, v) for v in value]))
-        if type(value) in [datetime.datetime, datetime.date, datetime.time]:
-            return "'%s'" % value
-        if type(value) is buffer:
-            return u"'[BLOB]'"
-        if column is None:
-            try:
-                return '%d' % int(value)
-            except ValueError:
-                return u"'%s'" % value
-        if (isinstance(column.type, Boolean)
-                and (type(value) is bool or value in ['true', 'false'])):
-            return '%s' % str(value).lower()
-        if isinstance(column.type, Float):
-            try:
-                return '%f' % float(value)
-            except ValueError:
-                pass
-        if isinstance(column.type, Integer):
-            try:
-                return '%d' % int(value)
-            except ValueError:
-                pass
-        if isinstance(column.type, TIMESTAMP):
-            try:
-                return '%d' % int(value)
-            except ValueError:
-                pass
-        return u"'%s'" % value.replace('%', '%%').replace("'", "''")
+        return format_value(column, value)
 
     def __setattr__(self, name, value):
         self.__dict__[name] = value
--- src/tests/options.py
+++ src/tests/options.py
@@ -3,7 +3,7 @@
 
 import unittest
 
-from tests.testcase import ParentTestCase
+from tests.testcase import DbTestCase
 from dbnav import options
 
 
@@ -13,7 +13,7 @@ def load_suite():
     return suite
 
 
-class OptionsTestCase(ParentTestCase):
+class OptionsTestCase(DbTestCase):
     def test_escape_keyword(self):
         """Tests the options.escape_keyword function"""
 
@@ -26,3 +26,44 @@ class OptionsTestCase(ParentTestCase):
         self.assertEqual(
             '"table"',
             options.escape_keyword('table'))
+
+    def test_format_value(self):
+        """Tests the options.format_value function"""
+
+        con = DbTestCase.connection
+        user = con.table('user')
+
+        self.assertEqual(
+            u'null',
+            options.format_value(None, None))
+        self.assertEqual(
+            u'7',
+            options.format_value(user.column('id'), 7))
+        self.assertEqual(
+            u"'a'",
+            options.format_value(user.column('id'), 'a'))
+        self.assertEqual(
+            'null',
+            options.format_value(user.column('id'), None))
+
+    def test_restriction(self):
+        """Tests the options.restriction function"""
+
+        con = DbTestCase.connection
+        user = con.table('user')
+
+        self.assertEqual(
+            u'a.id is null',
+            options.restriction('a', user.column('id'), '=', None))
+        self.assertEqual(
+            u'id is null',
+            options.restriction(None, user.column('id'), '=', None))
+        self.assertEqual(
+            u'a.id = 7',
+            options.restriction('a', user.column('id'), '=', 7))
+        self.assertEqual(
+            u"a.id = 'a'",
+            options.restriction('a', user.column('id'), '=', 'a'))
+        self.assertEqual(
+            u"id = 'a'",
+            options.restriction(None, user.column('id'), '=', 'a'))
